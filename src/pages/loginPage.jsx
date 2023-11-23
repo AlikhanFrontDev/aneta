e61c0a8809db987f59aa137c093b82bac59abf41
AlikhanFrontDev <alixanhellion@gmail.com>
2023-11-23 05:11:39 +0100

    hg

--- src/pages/loginPage.jsx
+++ src/pages/loginPage.jsx
@@ -76,7 +76,7 @@ export default function LoginPage() {
             onChange={(e) => setName(e.target.value)}
           /> */}
           <input
-            type="email"
+            type="text"
             placeholder="Username"
             onChange={(e) => setSurname(e.target.value)}
           />
